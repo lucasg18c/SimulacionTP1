e8abaedff98496941791881e28b33002eb763160
areartes4 <72423691+areartes4@users.noreply.github.com>
2021-03-24 17:08:42 -0300

    Corregido Tabla Valores Smirnov

--- Presentacion/FrmSmirnov.cs
+++ Presentacion/FrmSmirnov.cs
@@ -60,17 +60,21 @@ namespace SimulacionTP1.Presentacion
 
                 }
 
-                double abs = Math.Round(Math.Abs(acumPo - acumPe), 4);
+                
                 acumPo = Math.Round(count3 / cantNum, 4) + acumPo;
                 acumPe = Math.Round((totalIntervalos / cantNum), 4) + acumPe;
+                double abs = Math.Round(Math.Abs(acumPo - acumPe), 4);
+
+                if (abs >= maximo)
+                {
+                    maximo = abs;
+                }
+
                 dgvIntervalos.Rows.Add(Math.Round(count, 4), Math.Round(count2, 4), count3, totalIntervalos,
                     Math.Round(count3 / cantNum, 4), Math.Round(totalIntervalos / cantNum, 4), acumPo, acumPe,
                     abs,maximo);
 
-                if(abs >= maximo)
-                {
-                    maximo = abs;
-                }
+                
                 count = count + valorIntervalo;
                 count2 = count2 + valorIntervalo;
 
