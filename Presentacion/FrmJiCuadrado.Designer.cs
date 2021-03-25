4a48162bcb0abe06011a4bd8d59399938d2b12db
Martin Lerda <martinlerda21@gmail.com>
2021-03-25 20:22:22 -0300

    chi cuadrado

--- Presentacion/FrmJiCuadrado.Designer.cs
+++ Presentacion/FrmJiCuadrado.Designer.cs
@@ -3,14 +3,14 @@
     partial class FrmJiCuadrado
     {
         /// <summary>
-        /// Required designer variable.
+        /// Variable del diseñador necesaria.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Limpiar los recursos que se estén usando.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,27 +20,311 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tablaNumeros = new System.Windows.Forms.DataGridView();
+            this.iteracion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.serie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtCan = new System.Windows.Forms.TextBox();
+            this.txtInter = new System.Windows.Forms.TextBox();
+            this.gdrDesde = new System.Windows.Forms.DataGridView();
+            this.desde = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hasta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ne = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.c2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.histo = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tablaNumeros)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdrDesde)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histo)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(1104, 418);
+            this.btnGenerar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(129, 47);
+            this.btnGenerar.TabIndex = 21;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 17);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Intervalos Deseados";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Cantidad Numeros";
+            // 
+            // tablaNumeros
+            // 
+            this.tablaNumeros.AllowUserToAddRows = false;
+            this.tablaNumeros.AllowUserToDeleteRows = false;
+            this.tablaNumeros.AllowUserToResizeColumns = false;
+            this.tablaNumeros.AllowUserToResizeRows = false;
+            this.tablaNumeros.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.tablaNumeros.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.tablaNumeros.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.tablaNumeros.ColumnHeadersHeight = 30;
+            this.tablaNumeros.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.tablaNumeros.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iteracion,
+            this.serie});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.tablaNumeros.DefaultCellStyle = dataGridViewCellStyle2;
+            this.tablaNumeros.EnableHeadersVisualStyles = false;
+            this.tablaNumeros.Location = new System.Drawing.Point(10, 190);
+            this.tablaNumeros.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.tablaNumeros.MultiSelect = false;
+            this.tablaNumeros.Name = "tablaNumeros";
+            this.tablaNumeros.ReadOnly = true;
+            this.tablaNumeros.RowHeadersVisible = false;
+            this.tablaNumeros.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.tablaNumeros.Size = new System.Drawing.Size(211, 259);
+            this.tablaNumeros.TabIndex = 18;
+            // 
+            // iteracion
+            // 
+            this.iteracion.HeaderText = "Iteracion";
+            this.iteracion.Name = "iteracion";
+            this.iteracion.ReadOnly = true;
+            // 
+            // serie
+            // 
+            this.serie.HeaderText = "Serie";
+            this.serie.Name = "serie";
+            this.serie.ReadOnly = true;
+            // 
+            // txtCan
+            // 
+            this.txtCan.Location = new System.Drawing.Point(10, 65);
+            this.txtCan.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtCan.Name = "txtCan";
+            this.txtCan.Size = new System.Drawing.Size(157, 25);
+            this.txtCan.TabIndex = 22;
+            // 
+            // txtInter
+            // 
+            this.txtInter.Location = new System.Drawing.Point(10, 129);
+            this.txtInter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtInter.Name = "txtInter";
+            this.txtInter.Size = new System.Drawing.Size(157, 25);
+            this.txtInter.TabIndex = 23;
+            // 
+            // gdrDesde
+            // 
+            this.gdrDesde.AllowUserToAddRows = false;
+            this.gdrDesde.AllowUserToDeleteRows = false;
+            this.gdrDesde.AllowUserToResizeColumns = false;
+            this.gdrDesde.AllowUserToResizeRows = false;
+            this.gdrDesde.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gdrDesde.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gdrDesde.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.gdrDesde.ColumnHeadersHeight = 30;
+            this.gdrDesde.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.gdrDesde.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.desde,
+            this.hasta,
+            this.NO,
+            this.Ne,
+            this.c,
+            this.c2});
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI Semilight", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gdrDesde.DefaultCellStyle = dataGridViewCellStyle4;
+            this.gdrDesde.EnableHeadersVisualStyles = false;
+            this.gdrDesde.Location = new System.Drawing.Point(255, 16);
+            this.gdrDesde.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gdrDesde.MultiSelect = false;
+            this.gdrDesde.Name = "gdrDesde";
+            this.gdrDesde.ReadOnly = true;
+            this.gdrDesde.RowHeadersVisible = false;
+            this.gdrDesde.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.gdrDesde.Size = new System.Drawing.Size(520, 459);
+            this.gdrDesde.TabIndex = 24;
+            // 
+            // desde
+            // 
+            this.desde.HeaderText = "Inter. Desde";
+            this.desde.Name = "desde";
+            this.desde.ReadOnly = true;
+            // 
+            // hasta
+            // 
+            this.hasta.HeaderText = "Inter. Hasta";
+            this.hasta.Name = "hasta";
+            this.hasta.ReadOnly = true;
+            // 
+            // NO
+            // 
+            this.NO.HeaderText = "Nros obt";
+            this.NO.Name = "NO";
+            this.NO.ReadOnly = true;
+            // 
+            // Ne
+            // 
+            this.Ne.HeaderText = "Nros esp";
+            this.Ne.Name = "Ne";
+            this.Ne.ReadOnly = true;
+            // 
+            // c
+            // 
+            this.c.HeaderText = "C";
+            this.c.Name = "c";
+            this.c.ReadOnly = true;
+            // 
+            // c2
+            // 
+            this.c2.HeaderText = "C (AC)";
+            this.c2.Name = "c2";
+            this.c2.ReadOnly = true;
+            // 
+            // histo
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.histo.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.histo.Legends.Add(legend1);
+            this.histo.Location = new System.Drawing.Point(798, 16);
+            this.histo.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.histo.Name = "histo";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.histo.Series.Add(series1);
+            this.histo.Size = new System.Drawing.Size(435, 324);
+            this.histo.TabIndex = 25;
+            this.histo.Text = "chart1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtCan);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtInter);
+            this.groupBox1.Controls.Add(this.tablaNumeros);
+            this.groupBox1.Location = new System.Drawing.Point(14, 16);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(234, 459);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos ";
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Enabled = false;
+            this.btnReiniciar.Location = new System.Drawing.Point(967, 418);
+            this.btnReiniciar.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(115, 47);
+            this.btnReiniciar.TabIndex = 27;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
             // 
             // FrmJiCuadrado
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(471, 353);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1247, 490);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.histo);
+            this.Controls.Add(this.gdrDesde);
+            this.Controls.Add(this.btnGenerar);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "FrmJiCuadrado";
             this.Text = "FrmJiCuadrado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tablaNumeros)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gdrDesde)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.histo)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView tablaNumeros;
+        private System.Windows.Forms.TextBox txtCan;
+        private System.Windows.Forms.TextBox txtInter;
+        private System.Windows.Forms.DataGridView gdrDesde;
+        private System.Windows.Forms.DataVisualization.Charting.Chart histo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iteracion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn serie;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn desde;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hasta;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Ne;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c;
+        private System.Windows.Forms.DataGridViewTextBoxColumn c2;
+        private System.Windows.Forms.Button btnReiniciar;
     }
 }
+
